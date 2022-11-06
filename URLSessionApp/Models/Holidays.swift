6167f210a449d08f93c88f80e99d5271427d2750
AlxGrshn <113042128+AlxGrshn@users.noreply.github.com>
2022-11-06 19:20:07 +0300

    added main app logic

---
+++ URLSessionApp/Models/Holidays.swift
@@ -0,0 +1,13 @@
+//
+//  Holidays.swift
+//  URLSessionApp
+//
+//  Created by Alexander Grishin on 06.11.2022.
+//
+
+struct Holidays: Decodable {
+    let date: String
+    let localName: String
+    let name: String
+    let countryCode: String
+}
--- URLSessionApp/ViewController.swift
+++ URLSessionApp/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchHolidays()
     }
-
-
 }
 
+extension ViewController {
+    private func fetchHolidays() {
+        guard let url = URL(string: "https://date.nager.at/api/v3/PublicHolidays/2022/RU") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let holidays = try decoder.decode([Holidays].self, from: data)
+                print(holidays)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+    }
+}
